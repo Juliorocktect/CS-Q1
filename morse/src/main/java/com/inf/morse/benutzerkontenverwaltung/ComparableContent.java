b0a8412e28c4d51eaa9bbb2ac220056e15c46dd0
v187nce <134378283+v187nce@users.noreply.github.com>
2024-04-26 09:30:32 +0000

    gelöscht

--- morse/src/main/java/com/inf/morse/benutzerkontenverwaltung/ComparableContent.java
+++
@@ -1,9 +0,0 @@
-package com.inf.morse.benutzerkontenverwaltung;
-
-public interface ComparableContent<ContentType>{
-    public boolean isGreater(ContentType pContent);
-
-    public boolean isEqual(ContentType pContent);
-
-    public boolean isLess(ContentType pContent);
-}
