bd8f7147d2f90b425bb3a3d8271edcc95f132ace
Julius <113830349+Juliorocktect@users.noreply.github.com>
2024-04-26 09:44:16 +0000

    Benutzerprofil

---
+++ morse/src/main/java/com/inf/morse/Benutzerprofil.java
@@ -0,0 +1,29 @@
+import com.inf.morse.include.*;
+
+public class Benutzerprofil implements ComparableContent<Benutzerprofil>{
+    private String name;
+    private int password;
+
+    public String getName() {
+        return name;
+    }
+    public int getPassword() {
+        return password;
+    }
+    Benutzerprofil(String pName,int pPassword){
+        name = pName;
+        password = pPassword;
+    }
+    public boolean isLess(Benutzerprofil pBenutzerprofil){
+        return name.compareTo(pBenutzerprofil.getName()) < 0;
+    }
+    public boolean isGreater(Benutzerprofil pBenutzerprofil){
+        return name.compareTo(pBenutzerprofil.getName()) > 0;
+    }
+    public boolean isEqual(Benutzerprofil pBenutzerprofil)
+    {
+        return name.compareTo(pBenutzerprofil) == 0;
+    }
+
+
+}
