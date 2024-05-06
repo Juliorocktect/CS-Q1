005579c364b671cbe9b961bbb2dfed6e5d73e0c0
v187nce <134378283+v187nce@users.noreply.github.com>
2024-05-06 13:29:26 +0000

    leseText

--- morse/src/main/java/com/zeichenverwaltung/Zeichenverwaltung.java
+++ morse/src/main/java/com/zeichenverwaltung/Zeichenverwaltung.java
@@ -1,8 +1,26 @@
 package com.zeichenverwaltung;
 
-public class Zeichenverwaltung {
-    public Zeichenverwaltung(){
+import com.inf.morse.include.BinarySearchTree;
 
+public class Zeichenverwaltung {
+
+    private BinarySearchTree<Zeichen> zeichenBaum;
+
+    public Zeichenverwaltung(){
+        zeichenBaum = new BinarySearchTree<Zeichen>();
+    }
+
+    public void leseText(String text){
+        char akt;
+        Zeichen z; 
+        for (int i = 0; i<text.length(); i++){
+            akt = text.charAt(i);
+            z = zeichenBaum.search(new Zeichen(akt)); 
+            if (z!= null)
+                z.incAnzahl();
+            else 
+                zeichenBaum.insert(new Zeichen(akt));
+        }
     }
     
 }
